962e6b2c10f5ad2c46a4119f3036439c03639e09
royarchl <royl.developer.2019@gmail.com>
2024-08-10 18:35:55 -0700

    Update main to test functionality

--- src/main.cc
+++ src/main.cc
@@ -3,33 +3,44 @@
 int main() {
   GapBuffer gap_buffer;
 
+  gap_buffer.TextInsert("Theaiak");
   gap_buffer.PrintBuffer();
 
-  gap_buffer.InsertCharacter('c');
-  gap_buffer.InsertCharacter('a');
-  gap_buffer.InsertCharacter('r');
-  gap_buffer.InsertCharacter('r');
-  gap_buffer.InsertCharacter('y');
+  for (int i = 0; i < 5; ++i) {
+    gap_buffer.MoveCharacterLeft();
+  }
+  gap_buffer.PrintBuffer();
 
+  gap_buffer.MoveCharacterRight();
+  gap_buffer.PrintBuffer();
+
+  gap_buffer.TextInsert("01234567");  // 16 (resize)
+  gap_buffer.PrintBuffer();
+
+  gap_buffer.CharacterInsert('w');  // 17
+  gap_buffer.PrintBuffer();
+
+  for (int i = 0; i < 20; ++i) {
+    gap_buffer.CharacterDelete();
+  }
+  gap_buffer.PrintBuffer();
+
+  // gap_buffer.LoadFile("example.txt");
+  // gap_buffer.PrintBuffer();
+
+  for (int i = 0; i < 8; ++i) {
+    gap_buffer.MoveCharacterLeft();
+  }
   gap_buffer.PrintBuffer();
 
   gap_buffer.CharacterBackspace();
-
+  gap_buffer.CharacterBackspace();
   gap_buffer.PrintBuffer();
 
-  gap_buffer.MoveCharacterLeft();
-  gap_buffer.MoveCharacterLeft();
-
-  gap_buffer.PrintBuffer();
-
-  gap_buffer.CharacterDelete();
-
+  for (int i = 0; i < 3; ++i) {
+    gap_buffer.MoveCharacterRight();
+  }
   gap_buffer.PrintBuffer();
 
   return 0;
 }
-
-/*
- * GapEnd: 19 ... -> 18 -> 17 ... -> 18
- *
- */
