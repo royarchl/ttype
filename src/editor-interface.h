06f0524791a681c266aa2bd0cdf0abe0c9c14fb9
royarchl <royl.developer.2019@gmail.com>
2024-08-10 18:34:17 -0700

    Update interface to match current, needed functionality

--- src/editor-interface.h
+++ src/editor-interface.h
@@ -3,33 +3,40 @@
 
 #include <string>
 
+/*
+ * I'm unsure of if some of these features should be stored in this class (which
+ * defines a structure for holding text) or the Editor class which would make
+ * use of the structure for text and lines.
+ */
+
 class EditorInterface {
  public:
   virtual ~EditorInterface() = default;
 
-  // File operations
-  // virtual bool LoadFile(const std::string& filename) = 0;
+  /* File operations */
+  virtual void LoadFile(const std::string& filename) = 0;
   // virtual bool SaveFile(const std::string& filename) = 0;
 
-  // Text operations
-  virtual void InsertCharacter(char character) = 0;
+  /* Text operations */
+  virtual void CharacterInsert(char character) = 0;
   virtual void CharacterBackspace() = 0;
   virtual void CharacterDelete() = 0;
   // virtual void CharacterSpecialDelete() = 0;
 
-  // Cursor operations
-  virtual void MoveLineUp() = 0;
-  virtual void MoveLineDown() = 0;
+  /* Cursor operations */
   virtual void MoveCharacterLeft() = 0;
   virtual void MoveCharacterRight() = 0;
+  // virtual void MoveLineUp() = 0;
+  // virtual void MoveLineDown() = 0;
 
-  // Selection operations
+  /* Selection operations */
+  virtual void TextInsert(const std::string& text) = 0;
+  // virtual void TextBackspace(std::size_t start, std::size_t length) = 0;
+  // virtual void TextDelete(std::size_t start, std::size_t length) = 0;
   // virtual void ClipboardCopy(std::size_t start, std::size_t length) = 0;
   // virtual void ClipboardPaste() = 0;
-  virtual void InsertText(const std::string& text) = 0;
-  virtual void DeleteText(std::size_t start, std::size_t length) = 0;
 
-  // Optional
+  /* Optional */
   // virtual void Undo() = 0;
   // virtual void Redo() = 0;
 };
